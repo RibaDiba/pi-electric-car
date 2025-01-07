258acd766d70b2af9c1d1e8b0ca9241eda847849
RibaDiba <abrmod74@gmail.com>
2025-01-07 16:29:27 -0500

    from macbook

--- MotorLib.h
+++ MotorLib.h
@@ -1,31 +1,44 @@
 #include <iostream>
 
 class MotorDriver {
-     public:
-          int PWM_pin;
-          int DIR_pin;
-          MotorDriver(int PWM_pin, int DIR_pin);
-          void setSpeed(int speed);
+public:
+    int PWM_pin;
+    int DIR_pin;
+    int I2C_channel;
+
+    MotorDriver(int PWM_pin, int DIR_pin, int I2C_channel);
+    void setSpeed(int speed, int dir);
+    void debug_driver(int time, int speed, MotorDriver* array[], int array_size);
 };
 
-MotorDriver::MotorDriver(int PWM_pin, int DIR_pin) {
-     this->PWM_pin = PWM_pin;
-     this->DIR_pin = DIR_pin;
-     pinMode(PWM_pin, OUTPUT_PWM);
-     pinMode(DIR_pin, OUTPUT);
+MotorDriver::MotorDriver(int PWM_pin, int DIR_pin, int I2C_channel)
+    : PWM_pin(PWM_pin), DIR_pin(DIR_pin), I2C_channel(I2C_channel) {
+    // pinMode(PWM_pin, PWM_OUTPUT);
+    // pinMode(DIR_pin, OUTPUT);
 }
 
 void MotorDriver::setSpeed(int speed, int dir) {
-     switch (dir) {
-          case 0: 
-               digitalWrite(DIR_pin, LOW);
-               break;
-          case 1: 
-               digitalWrite(DIR_pin, HIGH);
-               break;
-          default: 
-               std::cout << "Invalid direction" << std::endl;
-     }
-     analogWrite(PWM_pin, speed);
+    // Handle direction values
+    switch (dir) {
+    case 0:
+        // digitalWrite(DIR_pin, LOW);
+        break;
+    case 1:
+        // digitalWrite(DIR_pin, HIGH);
+        break;
+    default:
+        std::cout << "Invalid Direction Value" << std::endl;
+        return;
+    }
+
+    // analogWrite(PWM_pin, speed);
 }
 
+void MotorDriver::debug_driver(int time, int speed, MotorDriver* array[], int array_size) {
+    for (int i = 0; i < array_size; ++i) {
+        MotorDriver* obj = array[i];
+        obj->setSpeed(200, 0);
+        std::cout << obj->PWM_pin << std::endl;
+        // delay(time);
+    }
+}
--- main.cpp
+++ main.cpp
@@ -1,8 +1,19 @@
-#include <wiringPi>
+#include <iostream>
 #include "MotorLib.h"
 
+void startup() {
+    MotorDriver m1 = MotorDriver(1, 2, 3);
+    MotorDriver m2 = MotorDriver(2, 2, 3);
+    MotorDriver m3 = MotorDriver(3, 2, 3);
+    MotorDriver m4 = MotorDriver(4, 2, 3);
+
+    MotorDriver* Motors[4] = {&m1, &m2, &m3, &m4};
+
+    m1.debug_driver(2000, 200, Motors, 4);
+    std::cout << "Compiled Correctly";
+}
+
 int main() {
-    MotorDriver m1 = MotorDriver(1, 2);
-    std::cout << "Motor initialized" << std::endl;
+    startup();
     return 0;
 }
