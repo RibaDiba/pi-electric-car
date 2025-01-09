f6bc1e7b1507df40e22875b7aaf3f67d95168e70
Abir Modak <abrmod74@gmail.com>
2025-01-08 22:17:03 -0500

    working test code

--- main.cpp
+++ main.cpp
@@ -3,7 +3,7 @@
 #include <wiringPi.h>
 
 void test() {
-    MotorDriver m1 = MotorDriver(17, 18, 0);  // Create motor driver on pins 17 and 18
+    MotorDriver m1 = MotorDriver(0, 1, 0); 
     m1.setSpeed(100, 0);  // Spin Right
     std::cout << "Motor 1 spinning Right" << std::endl;
     delay(2000);  // Wait for 2 seconds
