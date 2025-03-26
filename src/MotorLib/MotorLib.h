2c2025ca6fb262d0150251997c87949200580870
ribadiba <abrmod74@gmail.com>
2025-03-26 18:54:06 -0400

    added sparks

--- src/MotorLib/MotorLib.h
+++ src/MotorLib/MotorLib.h
@@ -18,9 +18,21 @@ class MotorDriver {
         int DIR_pin;
         int I2C_channel;
 
+        // for new motor controllers
+        int LPWM_pin;
+        int RPWM_pin;
+
         MotorDriver(int PWM_pin, int DIR_pin, int I2C_channel);
+        MotorDriver (int LPWM_pin, int RPWM_pin);
+        MotorDriver (int PWM_pin);
         void setSpeed(int speed, int dir);
+        void sparkSetSpeed(int speed);
         void debug_driver(int time);
+        void calibrateSpark();
+
+        void setSpeedNew(int speed, int dir);
+    private: 
+        int mapToPWM(int speed);
 };
 
 class Motors {
